1617c27a839b26fd3b0b96e2106ac3c024cd86bc
Your Name <dpacoria@unsa.edu.pe>
2024-10-06 00:23:31 -0500

    Se agrego la funcionalidad de marcar un terreno con radio de 9km

--- src/screens/MapScreen.tsx
+++ src/screens/MapScreen.tsx
@@ -1,7 +1,9 @@
-import { Map, Marker, Popup } from 'mapbox-gl';
+import { Map } from 'mapbox-gl';
 import { useEffect, useRef } from 'react';
 import { usePlaces } from '../hooks/usePlaces';
 
+
+import * as turf from '@turf/turf';
 import 'mapbox-gl/dist/mapbox-gl.css';
 
 export const MapScreen = () => {
@@ -11,7 +13,7 @@ export const MapScreen = () => {
     const mapDiv = useRef<HTMLDivElement>(null);
     const mapInstance = useRef<Map | null>(null);
     const {isLoading, currentPosition} = usePlaces();
-    // const circleId = useRef<string>('');
+    const circleId = useRef<string>('');
 
     useEffect(() => {
         if(!isLoading && !mapInstance.current) {
@@ -30,6 +32,54 @@ export const MapScreen = () => {
                 console.log(lng, lat);
 
             });
+
+
+            const updateCircle = (e: Event) => {
+
+                if(mapInstance.current===null) return;
+
+                const {lat, lng} = e.lngLat;
+
+                const center = [lng, lat];
+                const radius = 0.9;
+                const options = { steps: 64, units: 'kilometers', properties: { foo: 'bar' } };
+                const circle = turf.circle(center, radius, options);
+
+                // add a circle on a map
+                if (circleId.current) {
+                    if (mapInstance.current.getLayer(circleId.current)) {
+                        mapInstance.current.removeLayer(circleId.current);
+                    }
+                    if (mapInstance.current.getSource(circleId.current)) {
+                        mapInstance.current.removeSource(circleId.current);
+                    }
+                }
+
+                const newCircleId = `circle-${Date.now()}`; // ID único para el círculo
+
+                // Agregar el círculo al mapa
+                mapInstance.current.addSource(newCircleId, {
+                    type: 'geojson',
+                    data: circle,
+                });
+
+                mapInstance.current.addLayer({
+                    id: newCircleId,
+                    type: 'fill',
+                    source: newCircleId,
+                    layout: {},
+                    paint: {
+                        'fill-color': '#00f', // Color del círculo
+                        'fill-opacity': 0.5,
+                    },
+                });
+
+                circleId.current = newCircleId;
+
+            };
+
+            mapInstance.current.on('click', updateCircle);
+
 
         }
 
